c17b9aab2ab65bb4ca1628c5d309ea2b436b9dad
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-05-29 13:57:52 +0200

    séparation des styles pour différents composants

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  sassOptions: {
+    includePaths: ["src/styles"],
+    // Colle ce code en haut de TOUS les fichiers .scss automatiquement
+    prependData: `@use "variables" as *;`,
+  },
 };
 
 export default nextConfig;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import Logo from '@/components/Logo';
+import '@/styles/nav.scss';
 
 const navItems = [
     { name: 'Accueil', href: '/' },
