5883a8dad2e5b869013c3aceb896bd7911530a9a
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-06-09 21:26:05 +0200

    icon de l'oeil pour le password

--- src/components/Forms/Input.jsx
+++ src/components/Forms/Input.jsx
@@ -1,16 +1,27 @@
+import {useState} from "react";
+import {Eye, EyeClosed} from 'lucide-react';
+
 export default function Input({label, type, value, checked, onChange, error = false, ...props})
 {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const inputType = type === "password" && passwordVisible ? 'text' : type;
     return(
         <div className="form-group">
             <label>{label}</label>
             <input
-                type={type}
+                type={inputType}
                 value={value}
                 checked={checked}
                 onChange={onChange}
                 className={error ? "input-error" : ""}
                 {...props}
             />
+            {type === "password" && (
+                <div className="eye-mdp" onClick={() => setPasswordVisible(!passwordVisible)}>
+                    {passwordVisible ? <Eye size={20} /> :  <EyeClosed size={20} />}
+                </div>
+            )}
         </div>
     )
 }
