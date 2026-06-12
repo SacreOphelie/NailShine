24337062572f87cadc3031ec09ac7d1092da859d
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-06-12 11:07:41 +0200

    creations des pages manquantes

---
+++ src/app/favoris/page.jsx
@@ -0,0 +1,8 @@
+export default function Favoris() 
+{
+    return(
+        <div className="slide" id="favoris">
+    
+        </div>
+    );
+}
---
+++ src/app/mes-rendez-vous/page.jsx
@@ -0,0 +1,8 @@
+export default function MesRendezVous() 
+{
+    return(
+        <div className="slide" id="mes-rendez-vous">
+    
+        </div>
+    );
+}
