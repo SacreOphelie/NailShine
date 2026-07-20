5c57fd77e11095ab8a62ef7ce59611fa1e6722bb
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-07-20 18:59:02 +0200

    essaie d'un calendrier

--- src/app/prendre-rdv/page.jsx
+++ src/app/prendre-rdv/page.jsx
@@ -10,6 +10,24 @@ import Button from '@/components/Button';
 import Input from '@/components/Forms/Input';
 import { TriangleAlert } from 'lucide-react';
 
+const Days = [
+    { name: "Lundi", date: "2026-01-01" },
+    { name: "Mardi", date: "2026-01-02" },
+    { name: "Mercredi", date: "2026-01-03" },
+    { name: "Jeudi", date: "2026-01-04" },
+    { name: "Vendredi", date: "2026-01-05" },
+    { name: "Samedi", date: "2026-01-06" }
+];
+
+const Hours = [
+    { day: "Lundi", hour: ["10h", "14h", "16h"] },
+    { day: "Mardi", hour: ["10h", "16h"] },
+    { day: "Mercredi", hour: ["10h"] },
+    { day: "Jeudi", hour: ["14h", "16h"] },
+    { day: "Vendredi", hour: ["10h"] },
+    { day: "Samedi", hour: [] }
+];
+
 export default function PrendreRdv() {
     const {isConnected, userProfil,loading} = useAuth();
     // Afficher les techniques disponibles
@@ -18,6 +36,12 @@ export default function PrendreRdv() {
     const [techniqueId , setTechniqueId] = useState('');
     // Gérer la case Nail art
     const [nailArt, setNailArt] = useState(false);
+
+    // les const pour le calendrier
+    const [selectedDay, setSelectedDay] = useState(null);
+    const [selectedHour, setSelectedHour] = useState(null);
+    // le texte de la prise du rendez-vous
+    const [confirmationMessage, setConfirmationMessage] = useState('');
 
     // Récupérer les techniques depuis la base de données
     useEffect(() => {
@@ -43,6 +67,16 @@ export default function PrendreRdv() {
     // Gestion des erreurs
     const [erreur, setErreur] = useState({});
 
+    // Logique de la sélection du jour et de l'heure
+    const handleTime = (day, hour) => {
+        setSelectedDay(day);
+        setSelectedHour(hour);
+
+        // Le message de confirmation se met à jour 
+        const confirmationText = `Vous avez pris rendez-vous pour le ${day.name} ${day.date} à ${hour}.`;
+        setConfirmationMessage(confirmationText);
+    };
+
     // Envoi du formulaire
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -56,6 +90,11 @@ export default function PrendreRdv() {
             console.log("Veuillez sélectionner une technique.");
         }
 
+        if(!selectedDay || !selectedHour)
+        {
+            erreurs.date = "Veuillez sélectionner un jour et une heure.";
+        }
+
         //Si l'objet contient au moins une erreur, on affiche tout et on stoppe l'envoi
         if (Object.keys(erreurs).length > 0) {
             setErreur(erreurs);
@@ -64,6 +103,9 @@ export default function PrendreRdv() {
 
         console.log("Technique sélectionnée :", techniqueId);
         console.log("Nail art :", nailArt);
+        console.log("Jour sélectionné :", selectedDay);
+        console.log("Heure sélectionnée :", selectedHour);
+
     }
 
     // Temps de chargement
@@ -116,6 +158,45 @@ export default function PrendreRdv() {
                             </div>
                         </div>
                         {erreur.techniqueId && <div className="error-message"><TriangleAlert size={20}/>{erreur.techniqueId}</div>}
+                    </div>
+                    <div className="calendrier">
+                        <div className="mois">
+                            <p>Janvier</p>
+                            <div className="navigation">
+                                <p>Semaine-1-7</p>
+                            </div>
+                        </div>
+                        <div className="grid-days">
+                            {Days.map(day => (
+                                <div key={day.name} className="column">
+                                    <div className="day-name">
+                                        <p>{day.name}</p>
+                                    </div>
+                                    <div className="grid-hours">
+                                        {Hours.find(slotDay => slotDay.day === day.name)?.hour.map(hour => {
+                                            const isSelected = selectedDay?.date === day.date && selectedHour === hour;
+                                            return(
+                                                < button 
+                                                    key={hour}
+                                                    type="button"
+                                                    className={`hour-button ${isSelected ? 'selected' : ''}`}
+                                                    onClick={() => handleTime(day, hour)}
+                                                >
+                                                    {hour}
+                                                </button>
+                                            );
+                                        })}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        {/* Affichage des erreurs de calendrier */}
+                        {erreur.date && <div className="error-message"><TriangleAlert size={20}/>{erreur.date}</div>}
+                        {confirmationMessage && (   
+                            <div className="message">
+                                <p>{confirmationMessage}</p>
+                            </div>
+                        )}
                     </div>
                     <div className="btn">
                         <Button text="Confirmer"/>
