d321853d8afa54c4d27f129d352729998b202137
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-07-30 12:10:32 +0200

    ajout de la page confirmation de rendez-vous

---
+++ src/app/confirmation-rdv/page.jsx
@@ -0,0 +1,12 @@
+import '@/styles/confirm-rdv.scss';
+
+export default function ConfirmationRdv()  
+{
+    return(
+        <div className="slide" id="confirmation-rdv">
+            <div className="box">
+                <h2>Votre rendez-vous est confirmé</h2>
+            </div>
+        </div>
+    );
+}
