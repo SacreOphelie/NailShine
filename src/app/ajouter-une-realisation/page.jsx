bfc9a160320780a57602bcc0806b75511f2f124d
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-08-07 15:28:07 +0200

    mise en place des champs pour ajouter une réalisation

--- src/app/ajouter-une-realisation/page.jsx
+++ src/app/ajouter-une-realisation/page.jsx
@@ -6,8 +6,9 @@ import { useAuth } from '@/config/Auth';
 import { useState, useEffect} from 'react';
 import { useRouter } from 'next/navigation';
 import {supabase} from '@/config/supabase';
+import Input from '@/components/Forms/Input';
 import Select from '@/components/Forms/Select';
-// import Input from '@/components/forms/Input';
+import Textarea from '@/components/Forms/Textarea';
 import Button from '@/components/Button';
 import { TriangleAlert } from 'lucide-react';
 
@@ -21,6 +22,37 @@ export default function AddRea()  {
     // State : clients
     const [clients, setClients] = useState([]);
     const [clientId, setClientId] = useState('');
+    // State date
+    const [date, setDate] = useState('');
+    // State saison
+    const [saison, setSaison] = useState('');
+    // State couleur
+    const [couleur, setCouleur] = useState('');
+    // State titre
+    const [titre, setTitre] = useState('');
+    // State description
+    const [description, setDescription] = useState('');
+
+    const saisons =[
+        {id:"printemps", nom:"Printemps"},
+        {id:"été", nom:"Été"},
+        {id:"automne", nom:"Automne"},
+        {id:"hiver", nom:"Hiver"}
+    ]
+    const couleurs =[
+        {id:"rouge", nom:"Rouge"},
+        {id:"rose", nom:"Rose"},
+        {id:"violet", nom:"Violet"},
+        {id:"bleu", nom:"Bleu"},
+        {id:"vert", nom:"Vert"},
+        {id:"jaune", nom:"Jaune"},
+        {id:"orange", nom:"Orange"},
+        {id:"blanc", nom:"Blanc"},
+        {id:"noir", nom:"Noir"},
+        {id:"argent", nom:"Argent"},
+        {id:"or", nom:"Or"},
+        {id:"multicolore", nom:"Multicolore"}
+    ]
 
     // Gestion des erreurs de formulaire
      const [erreur, setErreur] = useState({});
@@ -77,6 +109,15 @@ export default function AddRea()  {
         if(!clientId){
             erreurs.clientId = "Veuillez sélectionner un client.";
         }
+        if(!date){
+            erreurs.date = "Veuillez sélectionner une date.";
+        }
+        if(!saison){
+            erreurs.saison = "Veuillez sélectionner une saison.";
+        }
+        if(!couleur){
+            erreurs.couleur = "Veuillez sélectionner une couleur.";
+        }
 
         //Si l'objet contient au moins une erreur, on affiche tout et on stoppe l'envoi
         if (Object.keys(erreurs).length > 0) {
@@ -86,6 +127,11 @@ export default function AddRea()  {
 
         console.log(" Technique ID :", techniqueId);
         console.log(" Client ID :", clientId);
+        console.log(" Date :", date);
+        console.log(" Saison :", saison);
+        console.log(" Couleur :", couleur);
+        console.log(" Titre :", titre);
+        console.log(" Description :", description);
     }
 
     const techniquesPrincipales = techniques.filter(technique => !technique.nail_art);
@@ -101,21 +147,37 @@ export default function AddRea()  {
             <div className="box">
                 <h2>Ajouter une réalisation</h2>
                 <form onSubmit={handleSubmit}>
-                    <div className="container-form">
-                        <div className="row">
-                            <div className="client">
-                                 <Select nomSelect="Client" options={clients} value={clientId} onChange={(e) => setClientId(e.target.value)} placeholder="Choisissez le client." 
-                                error={!!erreur.clientId}
-                                />
-                                {erreur.clientId && <div className="error-message"><TriangleAlert size={20}/>{erreur.clientId}</div>}
-                            </div>
-                            <div className="technique">
-                                <Select nomSelect="Technique" options={techniquesPrincipales} value={techniqueId} onChange={(e) => setTechniqueId(e.target.value)} placeholder="Choisissez la prestation que vous souhaitez." 
-                                error={!!erreur.techniqueId}
-                                />
-                                {erreur.techniqueId && <div className="error-message"><TriangleAlert size={20}/>{erreur.techniqueId}</div>}
-                            </div>
+                    <div className="row">
+                        <div className="client">
+                                <Select nomSelect="Client" options={clients} value={clientId} onChange={(e) => setClientId(e.target.value)} placeholder="Client" 
+                            error={!!erreur.clientId}
+                            />
+                            {erreur.clientId && <div className="error-message"><TriangleAlert size={20}/>{erreur.clientId}</div>}
                         </div>
+                        <div className="technique">
+                            <Select nomSelect="Technique" options={techniquesPrincipales} value={techniqueId} onChange={(e) => setTechniqueId(e.target.value)} placeholder="Technique" 
+                            error={!!erreur.techniqueId}
+                            />
+                            {erreur.techniqueId && <div className="error-message"><TriangleAlert size={20}/>{erreur.techniqueId}</div>}
+                        </div>
+                    </div>
+                    <div className="date">
+                        <Input type="date" placeholder="Date de la réalisation" value={date} onChange={(e) => setDate(e.target.value)} error={!!erreur.date} />
+                        {erreur.date && <div className="error-message"><TriangleAlert size={20}/>{erreur.date}</div>}
+                    </div>
+                    <div className="row">
+                        <div className="saison">
+                            <Select nomSelect="Saison" options={saisons} value={saison} onChange={(e) => setSaison(e.target.value)} placeholder="Saison" />
+                        </div>
+                        <div className="couleur">
+                            <Select nomSelect="Couleur" options={couleurs} value={couleur} onChange={(e) => setCouleur(e.target.value)} placeholder="Couleur" />
+                        </div>
+                    </div>
+                    <div className="titre">
+                        <Input type="text" placeholder="Titre de la réalisation" value={titre} onChange={(e) => setTitre(e.target.value)} />
+                    </div>
+                    <div className="description">
+                        <Textarea placeholder="Description de la réalisation" value={description} onChange={(e) => setDescription(e.target.value)} />
                     </div>
                     <div className="btn">
                         <Button text="Ajouter"/>
