e4eea0ae5237a3c0d5c3a7248a01d3cc7fdc69d8
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-08-13 13:47:26 +0200

    repassage à sessionStorage

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -7,7 +7,7 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 // On crée l'instance unique
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
-    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+    storage: typeof window !== 'undefined' ? window.sessionStorage : undefined,
     autoRefreshToken: true,
     persistSession: true
   }
