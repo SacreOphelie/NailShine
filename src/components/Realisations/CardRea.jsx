b17b35efcccfda3aa79c238fda52901ebb91e7da
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-08-13 11:56:19 +0200

    modif de la date + affichage de l'image

--- src/components/Realisations/CardRea.jsx
+++ src/components/Realisations/CardRea.jsx
@@ -1,18 +1,28 @@
+"use client";
+
 /* eslint-disable @next/next/no-img-element */
 import '@/styles/cardRealisation.scss';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function CardRealisation({realisations }) {
+
+  // Modifier le format de la date (JJ/MM/AAAA)
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('fr-FR');
+  };
+
   return (
     <>
       {realisations?.map((item) =>(
-          <Link href={`/${item.id}`} className="cardRea" key={item.id}>
+          <Link href={`/${item.id}`} className="cardRea" key={item.id} style={{ backgroundImage: `url(${item.image_url})` }}>
               <div className="favoris">
                 <p>Mettre en favoris</p>
                 <img src="/icones/Icon_heart.png" alt="Favoris" />
               </div>
               <div className="dateRea">
-                  <p>{item.date}</p>
+                  <p>{formatDate(item.date)}</p>
               </div>
           </Link>
       ))}
