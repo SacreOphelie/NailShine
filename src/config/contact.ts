b37de66cad6f29b72468d9a98cfad0caf34bb8c9
Ophélie Sacré <ophelie.sacre@outlook.be>
2026-08-26 23:53:56 +0200

    ajout des contacts

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -1,9 +1,9 @@
 export const contact = {
-  mail: "tonmail@example.com",
+  mail: "nathalie.nailshine@hotmail.com",
   tel: "+32 123 45 67 89",
   lieu:"Tubize 1480, Belgique",
   adresse:"Rue des perroquets 17, Tubize 1480",
-  facebook: "https://www.facebook.com/tonprofil",
-  instagram: "https://www.instagram.com/tonprofil",
-  tiktok: "https://www.tiktok.com/@tonprofil",
+  facebook: "https://www.facebook.com/nathalies.nails",
+  instagram: "https://www.instagram.com/alicoque_/",
+  tiktok: "https://www.tiktok.com/@alicoque",
 };
